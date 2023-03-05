a42d87cf2d103190c0dc5183e5f37992a2b87a02
pasi0014 <pasi0014@algonquinlive.com>
2023-03-05 14:36:41 -0500

    Pushed develop branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Registration from './components/RegisterForm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Registration />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/RegisterForm/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Registration = ({ props }: any) => {
+    return (
+        <div>
+            Hello from Registration
+        </div>
+    )
+};
+
+export default Registration;
