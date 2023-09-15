4b3cb40b484d17c5a64109baba3ef79edbe3229d
Nazar Pasika <nazar.pasika1@gmail.com>
2023-09-15 19:36:04 -0400

    Fixed config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   import.meta.env.MODE === "production"
-    ? "http://31.220.59.154:3005"
+    ? "https://api.ucssottawa.com"
     : "http://localhost:3005";
 
 export default API_BASE_URL;
