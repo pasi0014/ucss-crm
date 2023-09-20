7a57c76c41172efc4085c91a0244d9328c055e8f
Nazar Pasika <nazar.pasika1@gmail.com>
2023-09-20 15:08:50 -0400

    qre

---
+++ assets/index-3b13870e.js
@@ -0,0 +1,1 @@
+import{r as t,j as e,S as a,m as s,H as r}from"./index-aa19a58a.js";const i=()=>(t.useState(!1),e(a.Fragment,{children:e(s,{textAlign:"left",my:5,p:3,children:e(r,{children:"Reservations"})})}));export{i as default};
